005017ec0ec8488d0581c3eca8f30b2a95a50c3b
gustavomarchesim <gustavomarchesim@outlook.com>
2024-05-16 14:24:38 -0300

    DepartmentDaoJDBC and DaoFactory update

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
@@ -33,10 +34,10 @@ public class Program {
         }
 
         System.out.println("\n=== TEST 4: Seller Insert ===");
-        // Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(),
-        // 4000.0, department);
-        // sellerDao.insert(newSeller);
-        // System.out.println("Inserted new Seller! New Id: " + newSeller.getId());
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(),
+                4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted new Seller! New Id: " + newSeller.getId());
 
         System.out.println("\n=== TEST 5: Seller Update ===");
         seller = sellerDao.findById(1);
